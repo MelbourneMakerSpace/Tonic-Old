0a7cb0e77e6304af0207e5920d55bff5a583a71b
sazbi <pierson.a@hotmail.com>
2017-07-23 01:28:26 -0400

    fixed plans bugs

--- src/app/components/plans-grid/plans-grid.component.ts
+++ src/app/components/plans-grid/plans-grid.component.ts
@@ -9,21 +9,12 @@ import { PlansData } from "../../models/plans-data.class";
   styleUrls: ["./plans-grid.component.css"]
 })
 export class PlansGridComponent implements OnInit {
-<<<<<<< HEAD
 	data: PlansData[] = [
 		{ name: "Full", start: new Date(2017, 7, 22), end: new Date(2017, 12, 31) },
 		{ name: "Student", start: new Date(2017, 7, 22), end: new Date(2017, 12, 31) },
 		{ name: "Half", start: new Date(2017, 7, 22), end: new Date(2017, 12, 31) }
 		];
 		sortColumn: string = "Plan";
-=======
-  data: PlansData[] = [
-    { name: "Full", start: new Date(2017, 7), end: new Date(2017, 12) },
-    { name: "Student", start: new Date(2017, 7), end: new Date(2017, 12) },
-    { name: "Half", start: new Date(2017, 7), end: new Date(2017, 12) }
-  ];
-  sortColumn: string = "Plan";
->>>>>>> 03338e0a61d497777d62dafa16b9fb2230b742ad
 
   constructor() {}
 
